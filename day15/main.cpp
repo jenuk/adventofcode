78d4a74a172b9ba8c5a3afb9b535ebc2a963284c
jenuk <jonasjmueller@googlemail.com>
2021-12-16 00:18:34 +0100

    day 15: visualizing and bidirectional search

--- day15/main.cpp
+++ day15/main.cpp
@@ -26,9 +26,10 @@ class Grid {
         Grid();
         Grid(int f_n, int f_m);
 
-        std::vector<Position> get_neighbors(Position p) const;
+        std::vector<Position> get_neighbors(const Position& p) const;
 
         T distance(const Position& s, const Position& t) const;
+        T distance(const Position& s, const Position& t, const bool verbose) const;
 
         void set(const T& val, const Position& p);
         T operator[](Position p) const;
@@ -38,13 +39,17 @@ class Grid {
         Position topleft() const;
         Position bottomright() const;
 
+        Grid operator+(const Grid& other) const;
+        void write(std::string filename) const;
+
         friend std::istream& operator>>(std::istream& is, Grid<int>& grid);
 
         template <class S>
         friend class Grid;
 
     private:
-        Grid<bool> get_empty_mask() const;
+        template <class S>
+        Grid<S> get_empty_mask() const;
         std::vector<std::vector<T>> _data;
         int _n;
         int _m;
@@ -61,7 +66,7 @@ Grid<T>::Grid(int f_n, int f_m) : f_n(f_n), f_m(f_m) {}
 
 
 template <class T>
-std::vector<Position> Grid<T>::get_neighbors(Position p) const {
+std::vector<Position> Grid<T>::get_neighbors(const Position& p) const {
     std::vector<Position> res;
 
     if (p.x > 0) {
@@ -80,41 +85,92 @@ std::vector<Position> Grid<T>::get_neighbors(Position p) const {
     return res;
 }
 
-
 template <class T>
 T Grid<T>::distance(const Position& s, const Position& t) const {
+    return this->distance(s, t, false);
+}
+
+template <class T>
+T Grid<T>::distance(const Position& s, const Position& t, const bool verbose) const {
     typedef std::tuple<T, T, Position> Q_El;
     auto cmp = [](Q_El left, Q_El right) { return std::get<0>(left) > std::get<0>(right); };
-    std::priority_queue<Q_El, std::vector<Q_El>, decltype(cmp)> q(cmp);
+    std::priority_queue<Q_El, std::vector<Q_El>, decltype(cmp)> q_forward(cmp);
+    std::priority_queue<Q_El, std::vector<Q_El>, decltype(cmp)> q_backward(cmp);
 
-    Grid<bool> visited = this->get_empty_mask();
+    Grid<bool> visited_forward   = this->get_empty_mask<bool>();
+    Grid<int> distances_forward  = this->get_empty_mask<int>();
 
-    q.push(std::make_tuple(0, 0, s));
+    Grid<bool> visited_backward  = this->get_empty_mask<bool>();
+    Grid<int> distances_backward = this->get_empty_mask<int>();
+
+    q_forward.push(std::make_tuple(0, 0, s));
+    q_backward.push(std::make_tuple(0, 0, t));
 
     T dist, next_dist, next_adist;
     Position current;
+    int num_step = 0;
+    int freq = 500;
 
-    while (q.size() > 0) {
+    auto step = [&](std::priority_queue<Q_El, std::vector<Q_El>, decltype(cmp)>& q,
+                   Grid<bool>& visited, Grid<int>& distances, bool forward) {
         Q_El top = q.top();
         q.pop();
 
         dist    = std::get<1>(top);
         current = std::get<2>(top);
 
-        if (current == t) {
-            return dist;
-        }
         if (visited[current]) {
-            continue;
+            return;
         }
+        num_step++;
 
         visited.set(true, current);
+        distances.set(dist, current);
+
         std::vector<Position> neighbors = this->get_neighbors(current);
         for (int i=0; i<neighbors.size(); i++) {
-            next_dist  = dist + (*this)[neighbors[i]];
-            next_adist = next_dist + min_distance(neighbors[i], t);
+            if (forward) {
+                next_dist  = dist + ((*this)[neighbors[i]]-1)%9 + 1;
+                next_adist = next_dist + min_distance(neighbors[i], t);
+            } else {
+                next_dist  = dist + ((*this)[current]-1)%9 + 1;
+                next_adist = next_dist + min_distance(neighbors[i], s);
+            }
             q.push(std::make_tuple(next_adist, next_dist, neighbors[i]));
         }
+
+        return;
+    };
+
+    while (q_forward.size() > 0 or q_backward.size() > 0) {
+        if (q_forward.size() > 0) {
+            step(q_forward, visited_forward, distances_forward, true);
+            if (visited_forward[current] and visited_backward[current]) {
+                if (verbose) {
+                    (distances_forward + distances_backward).write("out/end");
+                }
+                return distances_forward[current] + distances_backward[current];
+            }
+            if (verbose and num_step%freq == 0) {
+                (visited_forward + visited_backward).write("out/" + std::to_string(num_step/freq));
+                num_step++;
+            }
+        }
+
+        if (q_backward.size() > 0) {
+            step(q_backward, visited_backward, distances_backward, false);
+            if (visited_forward[current] and visited_backward[current]) {
+                if (verbose) {
+                    (distances_forward + distances_backward).write("out/end");
+                }
+                return distances_forward[current] + distances_backward[current];
+            }
+            if (verbose and num_step%freq == 0) {
+                (visited_forward + visited_backward).write("out/" + std::to_string(num_step/freq));
+                num_step++;
+            }
+        }
+
     }
 
     // t not found
@@ -152,7 +208,7 @@ T Grid<T>::operator[](Position p) const {
         d++;
     }
 
-    return (this->_data[p.x][p.y] + d - 1)%9 + 1;
+    return this->_data[p.x][p.y] + d;
 }
 template <class T>
 void Grid<T>::set(const T& val, const Position& p) {
@@ -160,17 +216,90 @@ void Grid<T>::set(const T& val, const Position& p) {
 }
 
 template <class T>
-Grid<bool> Grid<T>::get_empty_mask() const {
-    Grid<bool> res;
+template <class S>
+Grid<S> Grid<T>::get_empty_mask() const {
+    Grid<S> res;
 
     for (int i=0; i<this->_n; i++) {
         res._data.push_back({});
         for (int j=0; j<this->_m; j++) {
-            res._data[i].push_back(false);
+            res._data[i].push_back(S());
         }
     }
 
+    res._n = this->_n;
+    res._m = this->_m;
+
     return res;
+}
+
+
+template <class T>
+Grid<T> Grid<T>::operator+(const Grid<T>& other) const {
+    // assert(this->_n == other._n and this->_m == other._m);
+
+    Grid<T> result;
+    result._n = this->_n;
+    result._m = this->_m;
+
+    Position p;
+    for (int i=0; i<this->_n; i++){
+        result._data.push_back({});
+        for (int j=0; j<this->_m; j++) {
+            p = Position{i, j};
+            result._data[i].push_back((*this)[p] + other[p]);
+        }
+    }
+
+    return result;
+}
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const Grid<T>& grid);
+
+template <>
+void Grid<int>::write(std::string filename) const {
+    std::ofstream file;
+    file.open(filename+".pgm");
+
+    file << "P2\n";
+    file << this->_n << " " << this->_m << "\n";
+    file << 3000 << "\n";
+
+    file << (*this);
+
+    file << std::flush;
+
+    file.close();
+}
+
+template <>
+void Grid<bool>::write(std::string filename) const {
+    std::ofstream file;
+    file.open(filename+".pbm");
+
+    file << "P1\n";
+    file << this->_n << " " << this->_m << "\n";
+
+    file << (*this);
+
+    file << std::flush;
+
+    file.close();
+}
+
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const Grid<T>& grid) {
+    Position end = grid.bottomright();
+    for (int i=0; i<=end.x; i++) {
+        for (int j=0; j<=end.y; j++) {
+            os << grid[Position{i, j}] << " ";
+        }
+        os << "\n";
+    }
+
+    return os;
 }
 
 
@@ -205,7 +334,7 @@ int main() {
 
     int res1 = grid.distance(grid.topleft(), grid.bottomright());
     grid.set_virtual_dim(5, 5);
-    int res2 = grid.distance(grid.topleft(), grid.bottomright());
+    int res2 = grid.distance(grid.topleft(), grid.bottomright(), true);
 
     std::cout << res1 << "\n";
     std::cout << res2 << std::endl;
