6a31c21f393a432b27a405d44ccbaa713a09bba7
jenuk <jonasjmueller@googlemail.com>
2022-12-01 10:43:50 +0100

    First day of 2022

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Advent of Code 2021
+# Advent of Code
 
-My solutions for the [Advent of Code 2021](https://adventofcode.com/2021). Solutions are written in C++ and maybe in Python using numpy. 
+My solutions for the [Advent of Code 2021](https://adventofcode.com/2021).
+Solutions are written in C++ and maybe in Python using numpy. 
+
+My solutions for the [Advent of Code 2022](https://adventofcode.com/2022).
+Solutions will likely be written in Python. 
---
+++ aoc2022/day01/main.py
@@ -0,0 +1,73 @@
+from time import perf_counter_ns
+
+
+# Ignore below
+
+def format_ns(time: int) -> str:
+    lengths = [1, 1000, 1000, 1000, 60, 60, 24]
+    units = ["ns", "μs", "ms", "s", "minutes", "hours", "days"]
+    idx = 1
+    prev = 0
+    while time > lengths[idx+1]:
+        idx += 1
+        time, prev = time//lengths[idx], time%lengths[idx]
+
+    out = f"{time}{units[idx]}"
+    if 0 < prev and time < 100:
+        out += f" {prev}{units[idx-1]}"
+    return out
+
+
+def read_data(filename="input.txt"):
+    with open(filename) as file:
+        content = file.read().strip()
+
+    return process_data(content)
+
+# Ignore above
+
+
+def process_data(content):
+    elves = content.split("\n\n")
+    elves = [map(int, elf.split("\n")) for elf in elves]
+    elves = list(map(sum, elves))
+    return elves
+
+
+def task1(data):
+    return max(data)
+
+
+def task2(data):
+    res = data[:3]
+    res.sort()
+    for cal in data[3:]:
+        if cal > res[0]:
+            if cal > res[2]:
+                res = [res[1], res[2], cal]
+            elif cal > res[1]:
+                res = [res[1], cal, res[2]]
+            else:
+                res = [cal, res[1], res[2]]
+
+    return sum(res)
+
+
+def main():
+    # fn = "input_bsp.txt"
+    fn = "input.txt"
+    t0 = perf_counter_ns()
+    data = read_data(fn)
+    t1 = perf_counter_ns()
+    result1 = task1(data)
+    t2 = perf_counter_ns()
+    result2 = task2(data)
+    t3 = perf_counter_ns()
+
+    print(f"Data preprocessing in {format_ns(t1 - t0)}")
+    print(f"Task 1: {result1} in {format_ns(t2 - t1)}")
+    print(f"Task 2: {result2} in {format_ns(t3 - t2)}")
+
+
+if __name__ == "__main__":
+    main()
